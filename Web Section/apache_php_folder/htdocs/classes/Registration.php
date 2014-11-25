f775f02c4d89e0c63eed952b38e34fdeed3f590b
HarryCordewener <admin@twilightdays.org>
2014-11-25 17:18:24 -0600

    File renames to import.

--- Web Section/apache_php_folder/htdocs/classes/Registration.php
+++ Web Section/apache_php_folder/htdocs/classes/Registration.php
@@ -1,15 +1,15 @@
 <?php
 
+include 'Post.php';
+
 /**
  * Class registration
  * handles the user registration
  */
 class Registration
 {
-    /**
-     * @var object $db_connection The database connection
-     */
-    private $db_connection = null;
+    private $registerurl = '/Register_Me' ;
+
     /**
      * @var array $errors Collection of error messages
      */
@@ -36,6 +36,7 @@ class Registration
      */
     private function registerNewUser()
     {
+        $telephone = "000-0000-000"; // Not implemented.
         if (empty($_POST['user_name'])) {
             $this->errors[] = "Empty Username";
         } elseif (empty($_POST['user_password_new']) || empty($_POST['user_password_repeat'])) {
@@ -65,50 +66,15 @@ class Registration
             && !empty($_POST['user_password_repeat'])
             && ($_POST['user_password_new'] === $_POST['user_password_repeat'])
         ) {
-            // create a database connection
-            $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-            // change character set to utf8 and check it
-            if (!$this->db_connection->set_charset("utf8")) {
-                $this->errors[] = $this->db_connection->error;
-            }
-
-            // if no connection errors (= working database connection)
-            if (!$this->db_connection->connect_errno) {
-
-                // escaping, additionally removing everything that could be (html/javascript-) code
-                $user_name = $this->db_connection->real_escape_string(strip_tags($_POST['user_name'], ENT_QUOTES));
-                $user_email = $this->db_connection->real_escape_string(strip_tags($_POST['user_email'], ENT_QUOTES));
-
-                $user_password = $_POST['user_password_new'];
-
-                // crypt the user's password with PHP 5.5's password_hash() function, results in a 60 character
-                // hash string. the PASSWORD_DEFAULT constant is defined by the PHP 5.5, or if you are using
-                // PHP 5.3/5.4, by the password hashing compatibility library
-                $user_password_hash = password_hash($user_password, PASSWORD_DEFAULT);
-
-                // check if user or email address already exists
-                $sql = "SELECT * FROM users WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_email . "';";
-                $query_check_user_name = $this->db_connection->query($sql);
-
-                if ($query_check_user_name->num_rows == 1) {
-                    $this->errors[] = "Sorry, that username / email address is already taken.";
-                } else {
-                    // write new user's data into database
-                    $sql = "INSERT INTO users (user_name, user_password_hash, user_email)
-                            VALUES('" . $user_name . "', '" . $user_password_hash . "', '" . $user_email . "');";
-                    $query_new_user_insert = $this->db_connection->query($sql);
-
-                    // if user has been added successfully
-                    if ($query_new_user_insert) {
-                        $this->messages[] = "Your account has been created successfully. You can now log in.";
-                    } else {
-                        $this->errors[] = "Sorry, your registration failed. Please go back and try again.";
-                    }
-                }
+            $truefalse = GetPost($loginurl, array( "Username" => $_POST['user_name'], 
+                                                    "Email" => $_POST['user_email'], 
+                                                    "Telephone" => $_POST['user_phone'], 
+                                                    "Password" => $_POST['user_password_new'] )); 
+            if( $truefalse == "true" ) {
+                $this->messages[] = "Your account has been created successfully. You can now log in.";
             } else {
-                $this->errors[] = "Sorry, no database connection.";
-            }
+                $this->errors[] = $truefalse;
+            } 
         } else {
             $this->errors[] = "An unknown error occurred.";
         }
