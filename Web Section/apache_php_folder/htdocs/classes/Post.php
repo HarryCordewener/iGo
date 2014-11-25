9a6569c99acca0061dd11f19e89f0ed205c9410f
HarryCordewener <admin@twilightdays.org>
2014-11-25 11:56:55 -0600

    Creation of a standard POST system for PHP - this requires CURL!
    Work on implementing against Login.

---
+++ Web Section/apache_php_folder/htdocs/classes/Post.php
@@ -0,0 +1,23 @@
+function GetPost( $url, $fields ) {
+	//url-ify the data for the POST
+	foreach($fields as $key=>$value) { $fields_string .= $key.'='. urlencode($value).'&'; }
+	rtrim($fields_string, '&');
+
+	//open connection
+	$ch = curl_init();
+
+	//set the url, number of POST vars, POST data
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_POST, count($fields));
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+	//execute post
+	$result = curl_exec($ch);
+
+	//close connection
+	curl_close($ch);
+    } else {
+        $this->errors[] = "Database connection problem.";
+    }
+    return $result;
+}
