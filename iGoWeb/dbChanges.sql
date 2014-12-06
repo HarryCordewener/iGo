abddffe433bb71bfaabe4abc1f4521bef9f5633d
lgunda2 <lgunda2@uic.edu>
2014-12-06 15:22:35 -0600

    Syncing files as in dropbox

--- iGoWeb/dbChanges.sql
+++ iGoWeb/dbChanges.sql
@@ -1,3 +1,8 @@
+ALTER TABLE `igo`.`restaurants` 
+CHANGE COLUMN 
+
+`restrauntid` `restrauntid` INT(11) NOT NULL AUTO_INCREMENT ;
+
 
 ALTER TABLE `igo`.`restaurants` 
 CHANGE COLUMN `restrauntid` `restrauntid` INT(11) NOT NULL AUTO_INCREMENT ;
@@ -11,3 +16,4 @@ ADD UNIQUE INDEX `name_UNIQUE` (`name` ASC);
 
 ALTER TABLE `igo`.`hospitals` 
 CHANGE COLUMN `name` `name` VARCHAR(200) NULL DEFAULT NULL ;
+
